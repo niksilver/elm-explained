f1c6ec94da52fdbfaa01c8aa3488f31e6dd73118
Nik Silver <nik@niksilver.com>
2015-12-16 21:54:08 +0000

    More on union types.

--- UnionTypes.elm
+++ UnionTypes.elm
@@ -2,31 +2,58 @@ module UnionTypes where
 
 -- A simple union type allows us to define an enumeration, like this
 
-type Suit = Hearts | Diamonds | Clubs | Spades
+type Direction = North | East | South | West
 
 {- In the above example we've defined the type Suit, but we've also
-   defined the values Hearts, Diamonds, Clubs and Spades, and we can
+   defined the values North, East, South and West, and we can
    use them now, like in the REPL:
 
-   > import UnionTypes
    > import UnionTypes exposing (..)
-   > Hearts
-   Hearts : UnionTypes.Suit
+   > East
+   East : UnionTypes.Direction
    >
 
-   You can see that Hearts is a value of type Suit.
+   You can see that East is a value of type Direction.
 -}
 
 -- Here's an example of using this
 
-describe : Suit -> String
-describe s
-  = case s of
-          Hearts -> "Red and bumpy"
-          Diamonds -> "Red and pointy"
-          Clubs -> "Black and bumpy"
-          Spades -> "Black and pointy"
+describe : Direction -> String
+describe dir
+  = case dir of
+          North -> "Cold"
+          East -> "Arid"
+          South -> "Hot"
+          West -> "Humid"
 
 -- Note that the above won't compile if we miss out one of the suits,
 -- because the compiler expects an exhaustive match.
+
+-- Union types also allow us to use more complex constructors:
+
+type Character = Ace | King | Queen | Jack
+type Value = Pips Int | Name Character
+type Card = Heart Value
+    | Diamond Value
+    | Club Value
+    | Spade Value
+    | Joker String
+
+d10 = Diamond (Pips 10)
+hking = Heart (Name King)
+joker1 = Joker "Laughing Jeremy"
+
+{- Let's look at the values' types:
+
+
+   > import UnionTypes exposing (..)
+   > d10
+   Diamond (Pips 10) : UnionTypes.Card
+   > hking
+   Heart (Name King) : UnionTypes.Card
+   > joker1
+   Joker ("Laughing Jeremy") : UnionTypes.Card
+   >
+
+-}
 
